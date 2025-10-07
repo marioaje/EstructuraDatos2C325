40eaf33ff70e9740fdda8ebfd1b21849a174ccb4
marioaje <marioaje@gmail.com>
2025-10-06 18:43:49 -0600

    Update hilos.cpp

--- ProjectEstDato2/ProjectEstDato2/hilos.cpp
+++ ProjectEstDato2/ProjectEstDato2/hilos.cpp
@@ -2,23 +2,23 @@
 #include <thread>    //Libreria de hilos
 #include <semaphore> //Semaforo y  sincronizacion
 
-std::counting_semaphore<1> semaforo(1); // Semaforo de control
+counting_semaphore<1> semaforo(1); // Semaforo de control
 
 // acceso a los recurso compartidos
 int recursosCompartidos = 0;
 
 // Tareas de ejecucion de acceso
-void tarea(const std::string &nombre)
+void tarea(const string &nombre)
 {
 
-    for (int i = 0; i < 4; i++)
+    for (int i = 0; i < 3; i++)
     {
         semaforo.acquire(); // Esperar turno, recursos, hacer fila;
 
         recursosCompartidos++; // Contador de recursos
-        std::cout << nombre << "acceso al recurso: " << recursosCompartidos << std::endl;
+        cout << nombre << "acceso al recurso: " << recursosCompartidos << endl;
         semaforo.release(); // liberar los recursos
-        std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        this_thread::sleep_for(chrono::milliseconds(200));
     }
 }
 
@@ -26,15 +26,15 @@ int main()
 {
 
     // o recorrerlo dinamicamente y generarlo con un foreach
-    std::thread hilo1(tarea, "Nombre del Hilo 1 ");
-    std::thread hilo2(tarea, "Nombre del Hilo 2 ");
-    std::thread hilo3(tarea, "Nombre del Hilo 3 ");
+    thread hilo1(tarea, "Nombre del Hilo 1 ");
+    thread hilo2(tarea, "Nombre del Hilo 2 ");
+    thread hilo3(tarea, "Nombre del Hilo 3 ");
 
     hilo1.join();
     hilo2.join();
     hilo3.join();
 
-    std::cout << " Fin del recurso " << recursosCompartidos << std::endl;
+    cout << " Fin del recurso " << recursosCompartidos << endl;
 
     return 0;
 }
